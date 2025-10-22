be4f00d70163e86055a19ef22e7a7a2e69990e33
kleber-matos <klebermatos2019@gmail.com>
2025-10-22 10:27:24 -0300

    Commit inicial

---
+++ script.js
@@ -0,0 +1,44 @@
+// Aula 1 - 20/10/2025
+
+// console.log() serve para mostrar mensagens no console do navegador ou do VS Code.
+console.log("Olá Mundo!!!");
+console.log("Hello World!!!");
+
+// Variáveis são usadas para armazenar informações, como nomes, números, idades, etc.
+// Estrutura básica:
+// tipo da variável + nome da variável + valor
+
+// Existem 3 formas principais de criar variáveis:
+// var, let e const
+
+// let: pode mudar de valor ao longo do código
+let idade = 18;
+console.log(idade);
+
+idade = 19;
+console.log(idade);
+
+// Exemplo: let é ideal quando o valor pode mudar.
+let nome = "Marina";
+console.log(nome);
+
+// const: é uma variável que NÃO pode ser alterada depois de criada
+const cpf = 1234567899;
+console.log(cpf);
+
+const sobrenome = "Pereira";
+console.log(sobrenome);
+
+let quantidadeDeBlusas = 10;
+
+let n1 = 1;
+let n2 = 3;
+
+console.log(n1 + n2);
+
+n2 = "3";
+
+console.log(n1 + n2);
+
+// Continuação
+// Aula 2 - 22/10/205
